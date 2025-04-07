f084459a3060419be509448c3e63e76e2f440551
FKD13 <44001949+FKD13@users.noreply.github.com>
2025-04-08 00:18:20 +0200

    use environ variables

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-TELRAAM_URL: str = 'http://localhost:8080'
-LAP_SOURCE_ID: int = 0
-RONNY_COUNT: int = 7
-SLEEP_DURATION: int = 5
+import os
+
+TELRAAM_URL: str = os.environ.get('DD_TELRAAM_URL', 'http://localhost:8080')
+LAP_SOURCE_ID: int = int(os.environ.get('DD_LAP_SOURCE_ID', '0'))
+RONNY_COUNT: int = int(os.environ.get('DD_RONNY_COUNT', '7'))
+SLEEP_DURATION: int = int(os.environ.get('DD_SLEEP_DURATION', '5'))
